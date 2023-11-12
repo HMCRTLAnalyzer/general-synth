425843981a993506eef20a523deefd44db500ee3
Diego Herrera Vicioso <dherreravicioso@hmc.edu>
2023-11-12 00:09:34 +0000

    Changed clk structure to match structures in gen_synth.py

--- constraints.tcl
+++ constraints.tcl
@@ -6,15 +6,10 @@
 set CLOCK_PERIOD $::env(CLK_PERIOD)
 
 # this structure is required for all clock signals in the design. 
-create_clock -name     "dco_clk"                              \
+create_clock -name     "clk"                              \
              -period   "$CLOCK_PERIOD"                        \
              -waveform "[expr $CLOCK_PERIOD/2] $CLOCK_PERIOD" \
-             [get_ports dco_clk]
-
-create_clock -name     "lfxt_clk"                             \
-             -period   "$CLOCK_PERIOD"                        \
-             -waveform "[expr $CLOCK_PERIOD/2] $CLOCK_PERIOD" \
-             [get_ports lfxt_clk]
+             [get_ports clk]
 
 
 set_max_area $::env(MAX_AREA)
